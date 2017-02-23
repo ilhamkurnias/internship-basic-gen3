be9f69808fb1d2e257c34c0bf869b3824d8bff81
ilhamkurnias <ilhamkurnia1711@gmail.com>
2017-02-23 22:10:18 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,11 @@
+class mahasiswa(object):
+	nim = 00000
+	def data (self, nama, hobi, tingkat):
+		self.nama = nama
+		self.hobi = hobi
+		self.tingkat = tingkat
+	
+	def prestasi(self, ip, juaralomba, mengulang):
+		self.ip = ip
+		self.juaralomba = juaralomba
+		self.mengulang = mengulang
